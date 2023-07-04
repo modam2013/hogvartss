740d9421bd17e8d03943c24af7e5d57b46210509
Аня <grigorevna.antoninka@mail.ru>
2023-07-04 14:55:51 +0700

    Initial commit

---
+++ src/test/java/com/example/hogvartss/HogvartssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hogvartss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogvartssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
